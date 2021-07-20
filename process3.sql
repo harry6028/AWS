3cfe96a2a984b98d10d03813d7358ee596f3bf63
Sharma <u370282@global.ual.com>
2021-07-20 13:28:18 +0530

    check

--- process3.sql
+++ process3.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
 
---changeset guestt:199
-create table test199 ( 
+--changeset guestt:139
+create table test139 ( 
 id int primary key,
 namee varchar(255)
 );
 
---rollback drop table test199;
+--rollback drop table test139;
